b7b350a083d6881304a8af86796f7704e43526bb
sidharthamahendra <sidharthamahendra@gmail.com>
2025-12-01 11:10:16 +0530

    insert-at-begin

---
+++ insert-at-begin.py
@@ -0,0 +1,29 @@
+﻿class ListNode:
+    def __init__(self, data=0, next=None):
+        self.data = data
+        self.next = next
+
+
+class Solution:
+    def insert_at_begin(self, head, data):
+        new_node = ListNode(data)
+        new_node.next = head
+        return new_node
+
+
+def print_list(head):
+    cur = head
+    vals = []
+    while cur:
+        vals.append(str(cur.data))
+        cur = cur.next
+    print("->".join(vals))
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    head = None
+    head = sol.insert_at_begin(head, 10)
+    head = sol.insert_at_begin(head, 20)
+    head = sol.insert_at_begin(head, 30)
+    print_list(head)
